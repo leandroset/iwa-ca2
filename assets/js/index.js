73e40883188e0f095be43c7165c4179fdcc1ba0c
leandroset <104846750+leandroset@users.noreply.github.com>
2022-05-10 14:21:39 -0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/user/update/${data.username}`,
+        "url" : `https://student-management-portal-ca2.herokuapp.com/user/update/${data.username}`,
         "method" : "PATCH",
         "data" : data
     }
@@ -34,7 +34,7 @@ if(window.location.pathname == "/"){
         var username = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/user/delete/${username}`,
+            "url" : `https://student-management-portal-ca2.herokuapp.com/user/delete/${username}`,
             "method" : "DELETE"
         }
 
