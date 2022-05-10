c961e361e83b6c93224cdafa6a8629ff35282ea8
leandroset <104846750+leandroset@users.noreply.github.com>
2022-05-10 10:19:35 -0700

    Update util.js

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,4 @@
 module.exports ={
     mongodbUrl:"mongodb://leandro:leandro@cluster0-shard-00-00.h3ygb.mongodb.net:27017,cluster0-shard-00-01.h3ygb.mongodb.net:27017,cluster0-shard-00-02.h3ygb.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-gikmme-shard-0&authSource=admin&retryWrites=true&w=majority"   
-    ,backendUrl:"http://localhost:3000"
+    ,backendUrl:"https://student-management-portal-ca2.herokuapp.com"
 }
