1a7d28989bb5c7fc137240366335afba85fc493a
leandroset <104846750+leandroset@users.noreply.github.com>
2022-05-17 20:44:27 +0500

    Update util.js

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,5 @@
+
+require('dotenv').config();
 module.exports ={
-    mongodbUrl:"mongodb://leandro:leandro@cluster0-shard-00-00.h3ygb.mongodb.net:27017,cluster0-shard-00-01.h3ygb.mongodb.net:27017,cluster0-shard-00-02.h3ygb.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-gikmme-shard-0&authSource=admin&retryWrites=true&w=majority"   
-    ,backendUrl:"https://student-management-portal-ca2.herokuapp.com"
+    mongodbUrl:process.env.mongoAtlasUri
 }
